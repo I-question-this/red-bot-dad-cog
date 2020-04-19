ef0be2edfeed6164f2b2a2781008436a451790ad
Tyler Westland <tylerofthewest@gmail.com>
2020-04-18 22:19:44 -0400

    Does that bare minimum dading.
    Responds to "I'm <...>" messages.

---
+++ dad.py
@@ -0,0 +1,32 @@
+import discord
+
+from redbot.core import commands
+from redbot.core.bot import Red
+
+class Dad(commands.Cog):
+    """Dad cog"""
+
+    def __init__(self, bot: Red):
+        super().__init__()
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.message):
+        if isinstance(message.channel, discord.abc.PrivateChannel):
+            return
+        author = message.author
+        valid_user = isinstance(author, discord.Member) and not author.bot
+        if not valid_user:
+            return
+        if await self.bot.is_automod_immune(message):
+            return
+
+        if message.content.find("I'm") != -1:
+            # Get their name
+            their_name = message.content[message.content.find("I'm")+4:]
+            # Construct our response
+            response = f"Hello {their_name}, I'm Dad!"
+            # Send message
+            await message.channel.send(response)
+
