c877fa60a4088a6dcf776329ff2220ae2734eb83
Tyler Westland <tylerofthewest@gmail.com>
2020-07-23 14:19:17 -0400

    Simplifies importing of Joke objects.
    Much more concise and clear.

--- __init__.py
+++ __init__.py
@@ -1,26 +1,4 @@
-from abc import ABC
-import inspect
-import sys
-# Import all the files that are relevant joke files
-from .jokes import __all__ as all_joke_files
-
-# Dictionary to save registered Joke objects
-JOKES = dict()
-
-# Go through all the possible jokes.
-for joke_file in all_joke_files:
-    # Import this sub-package/file
-    __import__("dad.jokes", fromlist=[joke_file])
-    # Check each class in that sub-package/file
-    for class_name, obj in inspect.getmembers(sys.modules[
-            f"dad.jokes.{joke_file}"], inspect.isclass):
-        # Ensure that it's a proper XJoke file
-        if "Joke" in class_name and not "Joke" == class_name:
-            # Create an instance
-            inst = obj() 
-            # Register the joke
-            JOKES[inst.name] = inst
-
+from .jokes import JOKES
 
 # Set up Dad bot
 from .dad import Dad
--- jokes/__init__.py
+++ jokes/__init__.py
@@ -1,7 +1,26 @@
 import os
+import inspect
 import sys
 
-# Add all python files to the __all__ variable.
+from .joke import Joke
+
+# Add all python files to the __all__ variable, except for __init__.py
 __all__ = list(name.replace(".py", "") for name in
                (filter(lambda name: name != "__init__.py" and ".py" in name,
 os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".")))))
+
+# Dictionary to save registered Joke objects
+JOKES = dict()
+
+# Go through all modules in "dad.jokes" aka "here"
+for module in __all__:
+    # Import this package
+    __import__("dad.jokes", fromlist=[module])
+    # Check each class in the imported module for Joke objects
+    for class_name, cls in inspect.getmembers(sys.modules[
+            f"dad.jokes.{module}"], inspect.isclass):
+        # Ensure that it's a proper non-abstract Joke class
+        if issubclass(cls, Joke) and not cls == Joke:
+            obj = cls()
+            # Register the joke
+            JOKES[obj.name] = obj
