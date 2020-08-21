b24f973d42f449f501d745dcf5f332ecf5e122e0
Tyler Westland <tylerofthewest@gmail.com>
2020-08-21 13:57:10 -0400

    Concertizes imports of Jokes.
    The implicit importing was causing compatibility
      problems.

--- jokes/__init__.py
+++ jokes/__init__.py
@@ -3,25 +3,20 @@ import inspect
 import sys
 
 from .joke import Joke
-
-# Add all python files to the __all__ variable, except for __init__.py
-__all__ = list(name.replace(".py", "") for name in
-               (filter(lambda name: name != "__init__.py" and ".py" in name,
-os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".")))))
+from .chores import ChoreJoke
+from .her import HerJoke
+from .i_am_dad import IAmDadJoke
+from .ok_boomer import OkBoomerJoke
+    from .rank import RankJoke
+    from .smashing import SmashingJoke
 
 # Dictionary to save registered Joke objects
-JOKES = dict()
-
-# Go through all modules in "dad.jokes" aka "here"
-for module in __all__:
-    # Import this package
-    __import__("dad.jokes", fromlist=[module])
-    # Check each class in the imported module for Joke objects
-    for class_name, cls in inspect.getmembers(sys.modules[
-            f"dad.jokes.{module}"], inspect.isclass):
-        # Ensure that it's a proper non-abstract Joke class
-        if issubclass(cls, Joke) and not cls == Joke:
-            obj = cls()
-            # Register the joke
-            JOKES[obj.name] = obj
+JOKES = {
+    "ChoreJoke": ChoreJoke(),
+    "HerJoke": HerJoke(),
+    "IAmDadJoke": IAmDadJoke(),
+    "OkBoomerJoke": OkBoomerJoke(),
+    "RankJoke": RankJoke(),
+    "SmashingJoke": SmashingJoke()
+}
 
