2d1cfebdb2ca06b2f86e65a9768f4a540b166392
Bayley King Windows <king2b3@mail.uc.edu>
2020-11-10 12:00:53 -0500

    respond to bonk
    Since the prediction library currently isn't working
    Dad in the meantime will just respond with a bonk gif
    whenever the keyword "bonk" is used

--- jokes/bonk.py
+++ jokes/bonk.py
@@ -13,6 +13,12 @@ class BonkJoke(Joke):
     def __init__(self):
         """Init for the bonk joke.
 
+        ** Please note **
+        The build for profanity_check is currently failing, 
+        so as of now Dad will just respond to the word "bonk"
+        until the library passes again.
+        ** **
+
         If someone gets a little NSFW, Dad will hastily correct them
         and send them to horny jail with a swift bonk.
         
@@ -29,7 +35,10 @@ class BonkJoke(Joke):
         
         # Set up super class
         super().__init__("bonk", 100.0)
+
         # Set up this class
+        self.bonk_re = re.compile(r"bonk", re.IGNORECASE)
+
 
 
     async def _make_verbal_joke(self, bot: Red, msg: discord.Message) -> bool:
@@ -47,10 +56,16 @@ class BonkJoke(Joke):
         bool
             Success of joke.
         """
-        
+        match = self.bonk_re.search(msg.content)
+        if match is None:
+            # No joke was possible, stop
+            return False
+        '''
+        # Uncomment to change dad to look for bad words
         if not predict([msg.content]):
             # Nothing wrong was said 
             return False
+        '''
         else:
             # Log joke
             LOG.info(f"Naughty: {msg.content}")
