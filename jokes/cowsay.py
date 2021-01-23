e28ae11d54b62bf72147fa9d97946f49338d590c
Tyler Westland <tylerofthewest@gmail.com>
2021-01-23 17:07:42 -0600

    Proof of concept for Cowsay
    It only has the cow and tux.
    More importantly it doesn't break up text into a
    specific width or even check if there are too many characters
    for a message.

---
+++ jokes/cowsay.py
@@ -0,0 +1,79 @@
+import discord
+import logging
+import random
+import re
+from redbot.core.bot import Red
+
+from .joke import Joke
+
+
+class CowSayJoke(Joke):
+    CHARACTERS = {
+            "cow":
+    """
+    \   ^__^
+     \  (oo)\_______
+        (__)\       )\/\\
+            ||----w |
+            ||     ||
+    """,
+            "tux":
+    """
+    \   .--.
+     \ |o_o |
+       |:_/ |
+      //   \ \\
+     (|     | )
+    /'\_   _/`\\
+    \___)=(___/
+    """
+        }
+
+    def __init__(self):
+        """Init for the Cowsay joke.
+
+        Cowsay is a program that generates an ASCII art cow with a speech
+        bubble containing user provided text. This is an incomplete recreation 
+        of that program in Python for Discord.
+        """
+        # Set up super class
+        super().__init__("cowsay", 1.0)
+        # Set up this class
+
+
+    @staticmethod
+    def speech_bubble(message):
+        bubble = "/" + "-"*len(message) + "\\\n"
+        bubble += f"|{message}|\n"
+        bubble += "\\" + "-"*len(message) + "/"
+        return bubble
+
+
+    async def _make_verbal_joke(self, bot: Red, msg: discord.Message) -> bool:
+        """Return byeah or bno
+
+        Parameters
+        ----------
+        bot: Red
+            The RedBot executing this function.
+        msg: discord.Message
+            Message to attempt a joke upon
+
+        Returns
+        -------
+        bool
+            Success of joke.
+        """
+        # Choose character
+        name, character = random.choice(list(self.CHARACTERS.items()))
+        # Log joke
+        self.log_info(msg.guild, msg.author, name)
+        # Send message
+        await msg.channel.send(
+                "```" + 
+                self.speech_bubble(msg.content) +
+                character +
+                "```")
+        # Return success
+        return True
+
